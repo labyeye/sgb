629dc8a2b0e7e5c1dcdb8079290bf97e423b86d4
Labh Bothra <labhbothra1380@gmail.com>
2025-07-06 20:03:14 +0530

    Done

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import { useState, useMemo } from "react";
 import ProductCard from "../components/ProductCard";
 import { Search, Filter, X } from "lucide-react";
 
